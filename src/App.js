118fb3a73743cecd8c33ceec22a1ebd73d2eeecf
shawlycode <orlandoshawly>
2021-06-17 14:18:56 +0000

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React, { Component } from 'react';
 
-import './App.css';
-import about from './About'
-
-function App() {
-  return (
-    <div className="App">
-    <App />,
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome Component!!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
